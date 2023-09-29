fb0d9a17574f30e6889281fa69266f0c3928612b
Anonymous_M21 <118679960+AnonymousM21@users.noreply.github.com>
2023-09-29 08:05:42 +0300

    Upgrading The Code Design
    made a little better design code on the input loop and added \n for 0 and 1 factorial.

--- factorial.c
+++ factorial.c
@@ -8,18 +8,15 @@ int main(void) {
     int i, j;
     long double fact = 1;
     // check if the entered value is -ve number and re-entering the value
-    while(1) {
+    do {
         printf("number to be factorilized : ");
         scanf("%i", &i);
         j = i;
-        if(i < 0) {
-            printf("only positive value! enter again.\n");
-            continue;
-        } else break;
-    }
+        if(i < 0) printf("only positive value! enter again.\n");
+    } while (i < 0);
     // 0 and 1 factorial is 1 as a rule
     if(i == 0 || i == 1) {
-        printf("%i! = 1", i);
+        printf("%i! = 1\n", i);
         return 0;
     }
     // calculate the factorial
